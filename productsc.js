4b4764a9dabb53482321a01edf495200604c2343
Igor Retesz <igorretesz@gmail.com>
2021-10-22 11:53:13 +0100

    Add cats.html and enabled the shop

---
+++ productsc.js
@@ -0,0 +1,32 @@
+var products = {
+    001: {
+      name : "cat tree",
+      desc : "Greatest tree for cats",
+      img : "chewy-I-rgDPLKogs-unsplash.jpg",
+      price : 48
+    },
+    002: {
+      name : "cat Food",
+      desc : "The cat food that every cat oves and needs.",
+      img : "fuu-j-FReocFYzRaA-unsplash.jpg",
+      price : 81
+    },
+    003: {
+      name : "cat toys",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "kim-davies-fEK4jvgnApg-unsplash.jpg",
+      price : 68
+    },
+    004: {
+      name : "boine",
+      desc : "Amazing loving puppy  ",
+      img : "alvan-nee-ZCHj_2lJP00-unsplash.jpg",
+      price : 1005
+    },
+    005:{
+        name: "hobs",
+        desc: "Waiting for a home",
+        img: "modcatshop-pdALzg0yN-8-unsplash.jpg",
+        price: 960
+    }
+  };
